29b87e15083acc685f329dd201401e6014575fb6
Leticia Fernandes <leticiafernandes777@gmail.com>
2021-02-20 19:03:14 -0300

    treating data

--- main.py
+++ main.py
@@ -7,13 +7,47 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.pipeline import Pipeline
 
+''' Importing Dataset '''
 df = pd.read_csv('data/complaints.csv')
 
+''' Treating Data '''
+#extracting columns
 df = df[['Product', 'Consumer complaint narrative']]
+#renaming Consumer complaint narrative column
+df = df.rename(columns = {'Consumer complaint narrative': 'Narrative'})
+#removing nil values for Narrative column
+df = df.loc[~df['Narrative'].isna()]
+#reseting the index of the DataFrame using drop=True to avoid the old index being added as a column
+df.reset_index(drop = True)
+#removing duplicate rows for Product
+df_aux = df['Product'].drop_duplicates(ignore_index = True)
+#renaming index column in place
+df_aux.rename(columns = {'index': 'Product_id'}, inplace = True)
+#merging df and df_aux with inner join
+df = df.merge(df_aux, on = 'Product', how= 'inner')
+#deleting Product column
+del df['Product']
 
-df.columns = ['Product', 'Narrative']
-
-X = df['Narrative'].to_list()
+''' Spliting Dataset into Train and Test '''
+X = df['Consumer complaint narrative'].to_list()
 y = df['Product'].to_list()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=11)
+
+clf = Pipeline([
+    ('count',CountVectorizer()),
+    ('tfidf',TfidfTransformer(use_idf=True)),
+    ('knn',KNeighborsClassifier())
+])
+
+knn_params = {
+    'knn__n_neighbors':np.arange(1,11),
+    'knn__metric': ['minkowski','cosine'],
+    'knn__n_jobs': [-1]
+}
+
+knn_model = GridSearchCV(clf, param_grid = knn_params, n_jobs = -1)
+knn_model.fit(X_train, y_train)
+
+preds = knn_model.predict(X_test)
+np.mean(preds == y_test)
